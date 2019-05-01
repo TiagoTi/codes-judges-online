91eca78614e13ed604902daa910bae885b5ec818
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-05-01 08:31:25 -0300

    add header to problem

--- sphere-online-judge/spoj-42/java/Main.java
+++ sphere-online-judge/spoj-42/java/Main.java
@@ -1,3 +1,29 @@
+/*
+EXPECT - Life, the Universe, and Everything (Interactive).
+
+Description
+		Your program is to use the brute-force approach in order to find the
+	Answer  to Life, the Universe, and Everything. More precisely... rewrite
+	small numbers from input to output. Stop processing input after reading in
+	the number 42. All numbers at input are integers of one or two digits
+
+    Interactive Protocol:
+				You should communicate with Judge using standard input and output.
+
+				Attention: the program should clear the output buffer after printing
+			each line.
+
+				It can be done using fflush(stdout) command or by setting the proper
+			type of buffering at the beginning of the execution - setlinebuf(stdout).
+
+				Each time the judge will give you a number. You should rewrite this
+			number to standard output. If this number equals 42, after rewriting your
+			program should terminate immediately.
+
+
+    Link: https://www.spoj.com/problems/EXPECT/
+
+*/
 import java.util.Scanner;
 
 public class Main {
