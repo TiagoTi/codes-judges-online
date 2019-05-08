f9dcddebb4b9e339d09f6db8a21131d37ff74588
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-05-07 22:18:14 -0300

    add sonar to python

---
+++ bin/sonar-python.sh
@@ -0,0 +1,9 @@
+path_name=$(readlink -f ./..)
+path_name=${path_name##*/}
+
+sonar-scanner \
+  -Dsonar.projectKey=$path_name \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=$SONAR_URL \
+  -Dsonar.login=$SONAR_LOGIN \
+  -Dsonar.language=py
